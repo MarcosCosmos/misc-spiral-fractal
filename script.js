7eed89b14ec5a16d634c5fb1335585c3ac09cab1
Marcos Cosmos <m.cosmos42@gmail.com>
2017-05-27 12:24:32 +1000

    tried changing color per shape instead of per line; set to default as new favourite

--- script.js
+++ script.js
@@ -3,6 +3,79 @@
 /* color conversions curtesy of https://gist.github.com/mjackson/5311256 */
 
 /**
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+ */
+function rgbToHsl(r, g, b) {
+  r /= 255, g /= 255, b /= 255;
+
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if (max == min) {
+    h = s = 0; // achromatic
+  } else {
+    var d = max - min;
+    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+    switch (max) {
+      case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+      case g: h = (b - r) / d + 2; break;
+      case b: h = (r - g) / d + 4; break;
+    }
+
+    h /= 6;
+  }
+
+  return [ h, s, l ];
+}
+
+/**
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the set [0, 1] and
+ * returns r, g, and b in the set [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hslToRgb(h, s, l) {
+  var r, g, b;
+
+  if (s == 0) {
+    r = g = b = l; // achromatic
+  } else {
+    function hue2rgb(p, q, t) {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1/6) return p + (q - p) * 6 * t;
+      if (t < 1/2) return q;
+      if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+      return p;
+    }
+
+    var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    var p = 2 * l - q;
+
+    r = hue2rgb(p, q, h + 1/3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [ r * 255, g * 255, b * 255 ];
+}
+
+
+/**
  * Converts an RGB color value to HSV. Conversion formula
  * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
  * Assumes r, g, and b are contained in the set [0, 255] and
@@ -100,22 +173,35 @@ $(function(){
     let target = $('canvas');
     let context = target[0].getContext('2d');
     var colourSwitch = false;
-    let drawLineInNextColor = (x, y) => {
 
-        let hueShift = 1.0001/initialShape.length;
-        // let hueShift = 1.0005/initialShape.length * ( colourSwitch ? 1.1 : 1);
-        // let hueShift = 1/initialShape.length;
-        // let hueShift = 0;
-        currentColor[0] = (currentColor[0] + hueShift) % 1.0;
+    let lineCount = 0;
+    // let hueShift = 1.0001/initialShape.length;
+    // let hueShift = 1.00006/initialShape.length * ( colourSwitch ? 1.1 : 1);
+    // let hueShift = 1/initialShape.length;
+    // let hueShift = 0;
+    let hueShift = 1/(2*initialShape.length*1000);
+    let drawLineInNextColor = (x, y) => {
+        if (lineCount % initialShape.length == 0) {
+            currentColor[0] = (currentColor[0] + hueShift) % 1.0;
+        }
+
+        // if (lineCount % (initialShape.length*100) == 0) {
+        //     hueShift = -hueShift;
+        // }
+
+
         let currentAsRgb = hsvToRgb(currentColor[0], currentColor[1], currentColor[2]).map((x) => Math.round(x));
         context.strokeStyle = 'rgb('+currentAsRgb.join(', ')+')';
 
         context.lineTo(x, y);
-        context.stroke();
-
+        // if (lineCount % (initialShape.length/3) == 0) {
+            context.stroke();
+        // }
         //set a new path so each is in a different color
         context.beginPath();
         context.moveTo(x,y);
+
+        lineCount += 1;
     };
 
     let waveAnchor = (function() {
@@ -176,7 +262,8 @@ $(function(){
         let newShape = [];
         let xDiff;
         let yDiff;
-        for(let curIdx = 0; curIdx < prevShape.length; ++curIdx) {
+        let curIdx = 0; //start pos
+        for(let i = 0; i < prevShape.length; ++i) {
             let nextIdx = curIdx < prevShape.length-1 ? curIdx+1 : 0;
             xDiff = prevShape[nextIdx][0] - prevShape[curIdx][0];
             yDiff = prevShape[nextIdx][1] - prevShape[curIdx][1];
@@ -190,6 +277,9 @@ $(function(){
 
             //draw the next point
             drawLineInNextColor(nextX, nextY);
+
+            //step to the next point, cycling back if neccessary
+            curIdx = (curIdx + 1) % prevShape.length;
         }
 
         drawLineInNextColor(newShape[0][0], newShape[0][1]);
